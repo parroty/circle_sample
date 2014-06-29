5851ffc2f4ba2249cbf0ed1b30d618890946c989
parroty <parroty00@gmail.com>
2014-06-29 13:21:09 +0900

    Fix settings - 4

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,8 +1,10 @@
 set -x
 set -e
+
+export PATH=`pwd`/otp_src_17.0/bin:$PATH
+
 if [ ! -e elixir/bin/elixir ]; then
   git clone https://github.com/elixir-lang/elixir
-  export PATH=`pwd`/../otp_src_17.0/bin:$PATH
   cd elixir
   make;
 fi
