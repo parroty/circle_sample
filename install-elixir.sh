762e152b1580de3651bc0533af71bbd64b4db9b6
parroty <parroty00@gmail.com>
2014-06-29 13:10:59 +0900

    Fix settings - 3

--- install-elixir.sh
+++ install-elixir.sh
@@ -2,7 +2,7 @@ set -x
 set -e
 if [ ! -e elixir/bin/elixir ]; then
   git clone https://github.com/elixir-lang/elixir
-  export PATH=`pwd`/elixir/bin:$PATH
+  export PATH=`pwd`/../otp_src_17.0/bin:$PATH
   cd elixir
   make;
 fi
--- install-otp_src_17.0.sh
+++ install-otp_src_17.0.sh
@@ -4,8 +4,6 @@ if [ ! -e otp_src_17.0/bin/erl ]; then
   curl -O http://www.erlang.org/download/otp_src_17.0.tar.gz
   tar xzf otp_src_17.0.tar.gz
   cd otp_src_17.0
-  export PATH=`pwd`/bin:$PATH
-
   ./configure --enable-smp-support \
               --enable-m64-build \
               --disable-native-libs \
